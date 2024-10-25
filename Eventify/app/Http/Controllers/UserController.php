af9b0a6c44ed2e745668e400a2b7a104b4fccb18
Deivi <david.fajardo.nogueroles@gmail.com>
2024-10-25 15:03:48 +0200

    Verificacion entera

--- Eventify/app/Http/Controllers/UserController.php
+++ Eventify/app/Http/Controllers/UserController.php
@@ -1,15 +1,9 @@
 namespace App\Http\Controllers;
 
-<<<<<<< Updated upstream
-
-use Illuminate\Http\Request;
-use App\Models\User;
-=======
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
->>>>>>> Stashed changes
 
 class UserController extends Controller
 {
@@ -39,41 +33,4 @@ class UserController extends Controller
 
         return response()->json(['message' => 'Usuario creado, verifica tu correo para continuar.']);
     }
-<<<<<<< Updated upstream
-    public function dashboard()
-    {
-    $users = User::where('deleted', 0)->get(); // Excluye los usuarios "soft deleted"
-    return view('admin.dashboard', compact('users'));
-    }
-    public function activate($id)
-    {
-        $user = User::findOrFail($id);
-
-        if ($user->email_confirmed && !$user->actived) {
-            $user->actived = 1;
-            $user->save();
-
-            return redirect()->route('admin.dashboard')->with('status', 'Usuario activado correctamente.');
-        }
-
-        return redirect()->route('admin.dashboard')->with('error', 'No se puede activar este usuario.');
-    }
-
-    public function deactivate($id)
-    {
-        $user = User::findOrFail($id);
-
-        if ($user->actived) {
-            $user->actived = 0;
-            $user->save();
-
-            return redirect()->route('admin.dashboard')->with('status', 'Usuario desactivado correctamente.');
-        }
-
-        return redirect()->route('admin.dashboard')->with('error', 'No se puede desactivar este usuario.');
-    }
-
 }
-=======
-}
->>>>>>> Stashed changes
